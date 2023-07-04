e88625f6c469ebd1a5bfdc5218d376c2d4ff76ec
Andrew Weng <78281843+HCWeng@users.noreply.github.com>
2023-07-04 06:43:01 +0000

    Update App.js array

--- src/App.js
+++ src/App.js
@@ -39,6 +39,16 @@ function App() {
           <h2>{concepts[0].title}</h2>
           <p>T{concepts[0].description}</p>
         </li>
+        <li className="concept">        
+          <img src={concepts[1].image} alt={concepts[1].title} />
+          <h2>{concepts[1].title}</h2>
+          <p>T{concepts[1].description}</p>
+        </li>
+        <li className="concept">        
+          <img src={concepts[2].image} alt={concepts[2].title} />
+          <h2>{concepts[2].title}</h2>
+          <p>T{concepts[2].description}</p>
+        </li>
       </ul>
     </div>
   );
